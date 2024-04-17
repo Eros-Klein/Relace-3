964f9993e9c37ad47d20c28c12a5e248af19e0cb
Daniel-Kirchmayr <d.kirchmayr@students.htl-leonding.ac.at>
2024-04-17 12:54:56 +0200

    CORS SPA Error
    CORS SPA Error muss gefixt werden dann muss die abfrage des Token kein 400 Error mehr sein

--- src/components/MicAPI.vue
+++ src/components/MicAPI.vue
@@ -9,84 +9,59 @@ export default {
       assignments: null,
     };
   },
-  async created() {
-    const code = this.getCodeFromUrl();
-    this.token = await this.getToken(code);
-    console.log('Token:', this.token);
-    //await this.sendTokenToBackend();
-    //this.assignments = await this.getAssignmentsFromBackend();
-  },
-
   methods: {
+    async authorizeAndFetchToken() {
+      await this.authorize();
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+      if (code) {
+        this.token = await this.getToken(code);
+      }
+    },
     async authorize() {
       const clientId = process.env.VUE_APP_CLIENT_ID;
       const redirectUri = process.env.VUE_APP_REDIRECT_URI;
+      const scope = 'https://graph.microsoft.com/.default';
 
-      console.log(redirectUri)
-
-      const authority =`https://login.microsoftonline.com/91fc072c-edef-4f97-bdc5-cfb67718ae3a`;
-      const authorizationUrl = `${authority}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=https://graph.microsoft.com/.default&state=12345`;
-
-      window.location.href = authorizationUrl;
-
-      let code = null;
-      while (!code) {
-        code = this.getCodeFromUrl();
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Warten Sie 1 Sekunde, bevor Sie erneut überprüfen
-      }
-
-      console.log(code);
-      this.token = await this.getToken(code);
-      console.log('Token:', this.token);
-
-    },
-    getCodeFromUrl() {
-      const urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get('code');
+      window.location.href = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}`;
     },
     async getToken(code) {
-      const axios = require('axios');
-      const params = new URLSearchParams();
-      params.append('client_id', process.env.VUE_APP_CLIENT_ID);
-      params.append('client_secret', process.env.VUE_APP_CLIENT_SECRET);
-      params.append('redirect_uri', process.env.VUE_APP_REDIRECT_URI);
-      params.append('grant_type', process.env.VUE_APP_GRANT_TYPE);
-      params.append('code', code);
+      const clientId = process.env.VUE_APP_CLIENT_ID;
+      const clientSecret = process.env.VUE_APP_CLIENT_SECRET;
+      const redirectUri = process.env.VUE_APP_REDIRECT_URI;
+      const tenantId = process.env.VUE_APP_TENANT_ID;
 
-      const config = {
+      const params = new URLSearchParams();
+      params.append('client_id', clientId);
+      params.append('scope', 'user.read mail.read');
+      params.append('code', code);
+      params.append('redirect_uri', redirectUri);
+      params.append('grant_type', 'authorization_code');
+      params.append('client_secret', clientSecret);
+
+      fetch(`https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`, { // Replace {tenant} with your tenant
+        method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-
-      try {
-        const response = await axios.post(`${process.env.VUE_APP_AUTHORITY}/oauth2/v2.0/token`, params, config);
-        return response.data.access_token;
-      } catch (error) {
-        console.error('Fehler beim Abrufen des Tokens:', error);
-      }
+        },
+        body: params
+      })
+          .then(response => response.json())
+          .then(data => {
+            const userToken = data.access_token;
+            // Use the user token here
+            console.log(userToken);
+          });
     },
-    /*
-    async sendTokenToBackend() {
-      try {
-        const response = await axios.get(process.env.VUE_APP_BACKEND_API_URL, {
-          headers: {
-            'Authorization': `Bearer ${this.token}`
-          }
-        });
-        console.log('Response:', response);
-      } catch (error) {
-        console.error('Fehler beim Senden des Tokens an das Backend:', error);
+  },
+  mounted() {
+    window.onload = () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const authorizationCode = urlParams.get('code');
+      if (authorizationCode) {
+        this.getToken(authorizationCode);
       }
-    },
-    async getAssignmentsFromBackend() {
-      try {
-        const response = await axios.get('http://localhost:3000/assignments');
-        return response.data;
-      } catch (error) {
-        console.error('Fehler beim Abrufen der Assignments Daten vom Backend:', error);
-      }
-    }*/
+    };
   }
 };
 </script>
@@ -96,7 +71,7 @@ export default {
     <h1>Token</h1>
     <p v-if="token">Token: {{ token }}</p>
     <p v-else>Loading token...</p>
-    <button @click="authorize">Authorize</button>
+    <button @click="authorizeAndFetchToken">Authorize</button>
   </div>
 </template>
 
